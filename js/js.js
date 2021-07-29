98ac977a0ef75aaf9c65946c15e9cb57b6317a17
Fernando <80068375+Megafer@users.noreply.github.com>
2021-07-29 16:57:35 -0300

    Add files via upload

--- js/js.js
+++ js/js.js
@@ -1,19 +1,19 @@
-<!-- script para que funcione al 100% el botón ir arriba -->
+//<!-- script para que funcione al 100% el botï¿½n ir arriba -->//
 
-//invocamos al objeto (window) y a su método (scroll), solo se ejecutara si el usuario hace scroll en la página
+//invocamos al objeto (window) y a su mï¿½todo (scroll), solo se ejecutara si el usuario hace scroll en la pï¿½gina
 $(window).scroll(function(){
-  if($(this).scrollTop() > 300){ //condición a cumplirse cuando el usuario aya bajado 301px a más.
-    $("#js_up").slideDown(300); //se muestra el botón en 300 mili segundos
+  if($(this).scrollTop() > 300){ //condiciï¿½n a cumplirse cuando el usuario aya bajado 301px a mï¿½s.
+    $("#js_up").slideDown(300); //se muestra el botï¿½n en 300 mili segundos
   }else{ // si no
-    $("#js_up").slideUp(300); //se oculta el botón en 300 mili segundos
+    $("#js_up").slideUp(300); //se oculta el botï¿½n en 300 mili segundos
   }
 });
 
-//creamos una función accediendo a la etiqueta i en su evento click
-$("#js_up i").on('click', function (e) { 
+//creamos una funciï¿½n accediendo a la etiqueta i en su evento click
+$("#js_up i").on('click', 'touchstart', function (e) { 
   e.preventDefault(); //evita que se ejecute el tag ancla (<a href="#">valor</a>).
-  $("body,html").animate({ // aplicamos la función animate a los tags body y html
-    scrollTop: 0 //al colocar el valor 0 a scrollTop me volverá a la parte inicial de la página
+  $("body,html").animate({ // aplicamos la funciï¿½n animate a los tags body y html
+    scrollTop: 0 //al colocar el valor 0 a scrollTop me volverï¿½ a la parte inicial de la pï¿½gina
   },700); //el valor 700 indica que lo ara en 700 mili segundos
   return false; //rompe el bucle
 });
